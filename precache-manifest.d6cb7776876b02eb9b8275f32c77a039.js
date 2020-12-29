458b74b3b079c6aa16c9f3357df7eeab5ec1e3ad
yamixa-gz <yamixa.gz@gmail.com>
2020-12-29 16:14:04 +0200

    Updates

---
+++ precache-manifest.d6cb7776876b02eb9b8275f32c77a039.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7719414656f82bccc82c",
+    "url": "/easyplace-public/static/css/main.758ec27e.chunk.css"
+  },
+  {
+    "revision": "7719414656f82bccc82c",
+    "url": "/easyplace-public/static/js/main.77194146.chunk.js"
+  },
+  {
+    "revision": "8ceeeeb7289296c2dcc8",
+    "url": "/easyplace-public/static/css/1.79763925.chunk.css"
+  },
+  {
+    "revision": "8ceeeeb7289296c2dcc8",
+    "url": "/easyplace-public/static/js/1.8ceeeeb7.chunk.js"
+  },
+  {
+    "revision": "b07b7700306ae47cba85",
+    "url": "/easyplace-public/static/js/2.b07b7700.chunk.js"
+  },
+  {
+    "revision": "df3ee37a0d6a0e4193d0",
+    "url": "/easyplace-public/static/js/runtime~main.df3ee37a.js"
+  },
+  {
+    "revision": "17bd5b3cfb58fe64a0a48dd97d1ba4b5",
+    "url": "/easyplace-public/static/media/avatar.17bd5b3c.svg"
+  },
+  {
+    "revision": "76999f8c9efac0b437eae54d7d543160",
+    "url": "/easyplace-public/static/media/rightArrow.76999f8c.svg"
+  },
+  {
+    "revision": "bd89de71c0c1dba659b6070ecf577c49",
+    "url": "/easyplace-public/static/media/leftArrow.bd89de71.svg"
+  },
+  {
+    "revision": "ef5485b32545294346101a8ec89ee382",
+    "url": "/easyplace-public/static/media/addBtnImg.ef5485b3.svg"
+  },
+  {
+    "revision": "bd96f72984e80947a8436761917926f0",
+    "url": "/easyplace-public/static/media/preloader_linear.bd96f729.svg"
+  },
+  {
+    "revision": "1547018a44abd4603d4f2230c8528103",
+    "url": "/easyplace-public/static/media/easyPlace.1547018a.svg"
+  },
+  {
+    "revision": "f5ac1466f3676af73e4a8f3cddab9fa2",
+    "url": "/easyplace-public/static/media/notRegistered.f5ac1466.svg"
+  },
+  {
+    "revision": "f1c4c62c7b837de9af698ce0cab1b026",
+    "url": "/easyplace-public/index.html"
+  }
+];
